3817bb1fcf426cee9205e24b681b4850c5e92e83
fpsy <plex@abv.bg>
2014-08-12 17:51:03 +0300

    Firs spider. Has some errors

---
+++ cfspider/cfspider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for cfspider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cfspider'
+
+SPIDER_MODULES = ['cfspider.spiders']
+NEWSPIDER_MODULE = 'cfspider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cfspider (+http://www.yourdomain.com)'
